f8ae1ce6836de1230c5daa041600e227fa81ab10
Wemerson Fernandes Rangel <wemersonf.rangel@outlook.com>
2021-08-31 14:55:48 -0300

    Atualiza a listagem de produtos após inserção de um novo produto

--- app/src/main/java/br/com/douglasmotta/whitelabeltutorial/ui/addproduct/AddProductViewModel.kt
+++ app/src/main/java/br/com/douglasmotta/whitelabeltutorial/ui/addproduct/AddProductViewModel.kt
@@ -7,6 +7,7 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import br.com.douglasmotta.whitelabeltutorial.R
+import br.com.douglasmotta.whitelabeltutorial.domain.model.Product
 import br.com.douglasmotta.whitelabeltutorial.domain.usecase.CreateProductUseCase
 import br.com.douglasmotta.whitelabeltutorial.util.fromCurrency
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -26,6 +27,9 @@ class AddProductViewModel @Inject constructor (private val createProductUseCase:
     private val _priceFieldErrorResId = MutableLiveData<Int?>()
     val priceFieldErrorResId: LiveData<Int?> = _priceFieldErrorResId
 
+    private val _productCreated = MutableLiveData<Product>()
+    val productCreated: LiveData<Product> = _productCreated
+
     private var isFormValid = false
 
     fun createProduct(description: String, price: String, imageUri: Uri?) = viewModelScope.launch {
@@ -38,6 +42,7 @@ class AddProductViewModel @Inject constructor (private val createProductUseCase:
         if (isFormValid) {
             try {
                 val product = createProductUseCase(description, price.fromCurrency(), imageUri!!)
+                _productCreated.value = product
             } catch (e: Exception) {
                 Log.d("Create Product", e.toString())
             }
